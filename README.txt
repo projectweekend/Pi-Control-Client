98179ba68a9d9fe72b1dcde580e3ff339e5293ef
Brian Hines <brian@projectweekend.net>
2015-01-03 09:08:51 -0600

    Pip it up

---
+++ README.txt
@@ -0,0 +1,7 @@
+==================
+Pi-Control-Client
+==================
+
+A client to controlling a Raspberry Pi running Pi-Control-Service: https://github.com/projectweekend/Pi-Control-Service
+
+For documentation: https://github.com/projectweekend/Pi-Control-Client
---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='Pi-Control-Client',
+    version='0.0.1',
+    author='Brian Hines',
+    author_email='brian@projectweekend.net',
+    packages=['pi_control_client'],
+    url='http://pypi.python.org/pypi/Pi-Control-Client/',
+    license='LICENSE.txt',
+    description='A client to controlling a Raspberry Pi running Pi-Control-Service (https://github.com/projectweekend/Pi-Control-Service).',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "pika == 0.9.14",
+    ],
+)
