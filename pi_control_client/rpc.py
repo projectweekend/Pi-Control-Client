6d26f758611cad3b2016fef89aece0aaa24efcf1
Brian Hines <brian@projectweekend.net>
2015-01-05 20:20:14 -0600

    Limit what is exposed

--- pi_control_client/rpc.py
+++ pi_control_client/rpc.py
@@ -9,37 +9,37 @@ class RPCClient(object):
         self.rabbit_url = rabbit_url
         self.queue_name = queue_name
         self.device_key = device_key
-        self.connection = pika.BlockingConnection(pika.URLParameters(self.rabbit_url))
+        self._connection = pika.BlockingConnection(pika.URLParameters(self.rabbit_url))
         self._setup_channel()
 
     def _setup_channel(self):
-        self.channel = self.connection.channel()
-        self.channel.exchange_declare(exchange=self.device_key, type='direct')
+        self._channel = self._connection.channel()
+        self._channel.exchange_declare(exchange=self.device_key, type='direct')
 
-        result = self.channel.queue_declare(exclusive=True)
-        self.rpc_response_queue = result.method.queue
+        result = self._channel.queue_declare(exclusive=True)
+        self._rpc_response_queue = result.method.queue
 
-        self.channel.basic_consume(
+        self._channel.basic_consume(
             self._handle_rpc_response,
             no_ack=True,
-            queue=self.rpc_response_queue)
+            queue=self._rpc_response_queue)
 
     def _handle_rpc_response(self, ch, method, props, body):
-        if self.correlation_id == props.correlation_id:
-            self.rpc_response = body
+        if self._correlation_id == props.correlation_id:
+            self._rpc_response = body
 
     def _call(self, message):
-        self.rpc_response = None
-        self.correlation_id = str(uuid.uuid4())
-        self.channel.basic_publish(
+        self._rpc_response = None
+        self._correlation_id = str(uuid.uuid4())
+        self._channel.basic_publish(
             exchange=self.device_key,
             routing_key=self.queue_name,
             properties=pika.BasicProperties(
-                reply_to=self.rpc_response_queue,
-                correlation_id=self.correlation_id),
+                reply_to=self._rpc_response_queue,
+                correlation_id=self._correlation_id),
             body=json.dumps(message))
 
-        while self.rpc_response is None:
-            self.connection.process_data_events()
+        while self._rpc_response is None:
+            self._connection.process_data_events()
 
-        return json.loads(self.rpc_response)
+        return json.loads(self._rpc_response)
